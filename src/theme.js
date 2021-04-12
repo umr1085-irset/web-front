a6e10adba79d9fcbca2194976e4566c683d6de97
root <root@genapp.novalocal>
2021-04-12 16:27:47 +0000

    styles for sliding pannel

--- src/theme.js
+++ src/theme.js
@@ -59,24 +59,34 @@ typography: {
       root: {
 		borderRadius: '0px',
       },
-	  withMargin: {
-		  margin: '10px'
-	  }
     },
 	MuiCard: {
       root: {
         padding:'0px',
+		marginBottom: '24px'
       },
     },
 	MuiCardHeader: {
       root: {
         backgroundColor:'#F9F9F9',
 		textTransform: 'Uppercase',
+		paddingBottom: '4px',
+		paddingTop: '8px',
       },
 	  title: {
-        fontSize : '12px',
+        fontSize : '13px',
 		fontColor: '#666666',
-		fontFamily : 'Quicksand'
+		fontFamily : 'Quicksand',
+		fontWeight: '400',
+		display: 'inline-block',
+      },
+	  subheader: {
+        fontSize : '11px',
+		fontColor: '#666666',
+		fontWeight: '400',
+		fontFamily : 'Quicksand',
+		display: 'inline-block',
+		marginLeft:'10px'
       },
     },
   },
