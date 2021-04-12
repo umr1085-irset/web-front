ea3de56a520c756422436abf25f45996e9c27a3b
root <root@genapp.novalocal>
2021-04-12 16:21:21 +0000

    Icons : Barchart,Doughnut,Pie

---
+++ src/assets/Icons/BarchartOutlined.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+
+
+function BarchartOutlinedIcon(props) {
+  return (
+    <SvgIcon {...props} viewBox="-4.125 -3.5 24 24">
+	/>
+<path d="M0,17.999V9h3.999v8.5v0.5H0L0,17.999z M3.499,17.499v-0.5V17.499L3.499,17.499z M1,16.999h1.999V9.998H1V16.999L1,16.999z"
+	/>
+<path d="M11.75,17.999v-12h3.998v11.5v0.5H11.75L11.75,17.999z M15.25,17.499v-0.5V17.499L15.25,17.499z M12.748,16.999h2.002
+	l-0.002-10h-2V16.999L12.748,16.999z"/>
+<path d="M5.999,17.999V0h3.999v17.499v0.5H5.999L5.999,17.999z M9.5,17.499v-0.5V17.499L9.5,17.499z M6.998,16.999H9L8.998,0.998h-2
+	V16.999L6.998,16.999z"/>
+    </SvgIcon>
+  );
+}
+
+export default BarchartOutlinedIcon;
+
+
---
+++ src/assets/Icons/SquareOutlined.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+
+
+function SquareOutlinedIcon(props) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 24 24">
+<path d="M0,24V0h24v23.5V24H0L0,24z M23.5,23.5V23V23.5L23.5,23.5z M1,23h22l0-22H1V23L1,23z"/>
+    </SvgIcon>
+  );
+}
+
+export default SquareOutlinedIcon;
+
+
