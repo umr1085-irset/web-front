d8bb70a29b561ac088af0e701751d48fb3396e07
noel <laurence.r.noel@gmail.com>
2021-05-21 18:37:59 +0200

    DotPlot Icon

---
+++ src/assets/Icons/DotPlot.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+
+
+function DotPlotIcon(props) {
+  return (
+    <SvgIcon {...props} viewBox="-3 -4.5 24 24">
+<circle cx="2.41" cy="2.41" r="2.41"/>
+<circle cx="2.41" cy="8.362" r="1.417"/>
+<circle cx="2.41" cy="14.032" r="1.417"/>
+<circle cx="8.66" cy="2.285" r="1.417"/>
+<circle cx="8.659" cy="8.237" r="0.708"/>
+<circle cx="8.66" cy="13.907" r="0.709"/>
+<circle cx="15.035" cy="2.285" r="0.708"/>
+<circle cx="15.034" cy="8.237" r="2.409"/>
+<circle cx="15.035" cy="13.907" r="1.417"/>
+    </SvgIcon>
+  );
+}
+
+export default DotPlotIcon;
+
+
