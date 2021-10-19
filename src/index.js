46c630fd92af0ab1f0db1d7a43f1a6f5ad0f6453
tdarde <thomas.darde@scilicium.com>
2021-10-19 15:07:20 +0200

    change baseUrl

--- src/index.js
+++ src/index.js
@@ -37,7 +37,9 @@ import theme from './theme';
 
 
 import axios from "axios";
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+//axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = "http://django:5000";
+
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
