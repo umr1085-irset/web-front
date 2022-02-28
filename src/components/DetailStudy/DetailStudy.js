589066578ee86495d89d8720dbfc3a057c70151d
root <root@appvisu.novalocal>
2022-02-28 09:13:01 +0000

    Specific attributes to display for datasets

--- src/components/DetailStudy/DetailStudy.js
+++ src/components/DetailStudy/DetailStudy.js
@@ -59,46 +59,49 @@ class DetailStudyPage extends Component {
 	            
             <MDBContainer fluid>
 	    <MDBRow>
-	    <MDBCol size="9">
-            
-                        <MDBTable borderless>
-                                <MDBTableBody>
-	    			{study.description?
-                                    <tr>
-                                        <td><Typography variant="h4" color="textSecondary">Description</Typography></td>
-                                        <td><Typography variant="body1">{study.description}</Typography>   </td>
-                                    </tr> : <span />}
-	                            
-	   			 {study.contributor.length?
-                                    <tr>
-                                        <td><Typography variant="h4" color="textSecondary">Contributors</Typography></td>
-                                        <td><Typography variant="body1">{study.contributor}</Typography>   </td>
-                                    </tr> : <span /> }
+	    <MDBCol size="8">
+            <Box style={{ margin: 16}}>
+          	    			{study.description?
+          <Typography variant="body1">{study.description}</Typography>  : <span />}
+	                         
                                                           
+	    {study.contributor.length?
+	<Box>		   
+	      <Typography color="textSecondary">Contributors : </Typography>   
+	         
+		   {study.contributor.map(function(contributor, idx){
+				          return(
+				                   <Typography variant="body1" key={"contributor_"+idx}>{study.contributor.name}</Typography>
+						 )
+				             })}
+	</Box>	
+		: <span />
+	     
+	}
 
-                                </MDBTableBody>
-                           </MDBTable>
-	         <Typography variant="overline"> Data curated by {study.created_by.username}, last updated: {moment(study.updated_at).fromNow()}</Typography>
+                 
+                
+	         <Typography variant="overline"> Data curated by {study.dataCurators}, last updated: {moment(study.updated_at).fromNow()}</Typography>
                        
-                    
+                    </Box>
                 </MDBCol>
-                <MDBCol size="3">
+                <MDBCol size="4">
                   
                           <Typography variant="body1">
 	    <div style={{ marginTop: 16 }}><Typography variant="h4" color="textSecondary">Related project</Typography></div> {study.collection.title} ({study.collection.projectId})
 	   
 	    {study.article.length?
-	          <Box>
-                  <Typography variant="h4" color="textSeondary">Related articles</Typography>
+	          <Box style={{ marginTop: 16 }}>
+                  <Typography variant="h4" color="textSecondary">Related article(s)</Typography>
 		   {study.article.map(function(article, idx){
 				          return(
-				                   <Box>  {article.title},  {moment(article.releaseDate).format('YYYY')}  <a key={"link_"+idx} href={"https://pubmed.ncbi.nlm.nih.gov/"+article.pmid}><Button key={"btn_"+idx} color="primary">{article.pmid}</Button></a>  </Box>
+				                   <Box>  {article.title}, {article.journal}, {moment(article.releaseDate).format('YYYY')} ,  <a key={"link_"+idx} href={"https://pubmed.ncbi.nlm.nih.gov/"+article.pmid}>pmid:{article.pmid}</a>  </Box>
 						 )
 				             })}
 		</Box>
 		:
 	       <span />
-	}
+	    }	
 	 			
 
                            </Typography> 
@@ -111,7 +114,7 @@ class DetailStudyPage extends Component {
            
                     <Card variant="outlined">
                         <CardHeader title="Associated datasets"></CardHeader>
-                        <CardContent>
+                        <CardContent style={{ marginBottom: -20  }}>
                             <MDBTable>
                                 <MDBTableBody>
                                 {study.dataset_of.map(function(data, idx){
@@ -119,11 +122,11 @@ class DetailStudyPage extends Component {
                                         <tr key={idx}>
                                             
                                             <td key={"id__"+idx} className="capitalize border-bottom border-top-0">{data.title} ({data.datasetId}) </td>
-                                            {data.bioMeta.tissue? <td className="border-bottom border-top-0" key={"tissue_"+idx}>{_.map(data.bioMeta.tissue, 'ontologyLabel').toString()}</td>:<td key={"tissue_"+idx} className="border-bottom border-top-0">{_.map(data.bioMeta.cell, 'name').toString()}</td>}
-                                            <td className="capitalize border-bottom border-top-0" key={"gender_"+idx}>{data.bioMeta.sex.join(", ")}</td>
-                                            {data.bioMeta.dev_stage? <td className="border-bottom border-top-0" key={"dev_stage_"+idx}>{_.map(data.bioMeta.dev_stage, 'ontologyLabel').toString()}</td>:<td className="border-bottom border-top-0" key={"dev_stage_"+idx}>No information provided</td>}
-                                            <td className="capitalize border-bottom border-top-0" key={"omics_"+idx}>{_.map(data.sop.omics, 'ontologyLabel').toString()}</td>
-                                            <td className="capitalize border-bottom border-top-0" key={"technology_"+idx}>{_.map(data.sop.technoGrain,'ontologyLabel').toString()} {_.map(data.sop.technology,'ontologyLabel').toString()}</td>
+
+				            <td className="border-bottom border-top-0" key={"info_"+idx}> <Typography variant="h4" color="textSecondary"> {data.loom.cellNumber?  <span>{data.loom.cellNumber} cells | </span> :<span/> }    {data.bioMeta.age_start? <span>{data.bioMeta.age_start}-{data.bioMeta.age_end} {data.bioMeta.age_unit} | </span>  :<span /> }  {_.map(data.bioMeta.organ,'ontologyLabel').toString()}  |   {_.map(data.sop.technology,'ontologyLabel').toString()}    </Typography></td>
+
+                                            
+                                            
                                             <td key={"btn_"+idx} align="right" className="border-bottom border-top-0">
                                                     <Link to={"/dataset/"+data.datasetId}><Button key={"display_btn"+idx} color="primary" size="medium" startIcon={<ScatterPlotTwoToneIcon />} variant="outlined"> Visualize </Button></Link>
                                             </td>
