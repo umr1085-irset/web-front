216b68e7bcdf6a0e02321f75db4dcf366ca2fe36
Laurence Noël <laurence.r.noel@gmail.com>
2022-06-16 09:28:55 +0000

    Removed Chromosome filter

--- src/components/Results/GeneExpressionMenuComponent.js
+++ src/components/Results/GeneExpressionMenuComponent.js
@@ -267,7 +267,7 @@ class GeneExpPlotMenuComponent extends Component {
 
                     <MDBCol md="8" className="pb-2 ml-4" >
                   <MDBRow >
-		   <MDBCol md="3">
+		   <MDBCol md="4">
                         <FormControl className="control-filter">
                             <InputLabel className="control-filter"  id="geneList">Gene selection</InputLabel>
                             <Select className="control-filter"
@@ -277,37 +277,15 @@ class GeneExpPlotMenuComponent extends Component {
                             value={this.state.method}
                             onChange={this.handleChangeGeneSelection}
                             >
-                            <MenuItem value="first" >first 10 genes</MenuItem>
-                            <MenuItem value="variance" >top 10 variable genes</MenuItem>
+                            <MenuItem value="first" >First 10 genes</MenuItem>
+                            //<MenuItem value="variance" >top 10 variable genes</MenuItem>
                             <MenuItem value="custom" >My selection</MenuItem>
                             </Select>
 
                         </FormControl>
 
 			</MDBCol>
-  
-            <MDBCol md="3">
-
-        
-                        <FormControl className="control-filter">
-                        <Autocomplete
-                                            limitTags={1}
-                                            id="chroms"
-                                            title="chroms"
-                                            size="small"
-                                            onChange={(event, newValue) => {
-                                                
-                                            }}
-                                            options={chroms}
-                                            renderInput={(params) => (
-                                            <TextField  {...params} label="From " placeholder="all chromosomes" style={{width:160}} />
-                                            )}
-                                        />
-                              </FormControl>
-
-			</MDBCol>
-
-			<MDBCol md="5">
+			<MDBCol md="6">
 
                 {this.state.gene
 			? <Spinner/>
@@ -315,7 +293,7 @@ class GeneExpPlotMenuComponent extends Component {
 
                                 <MDBRow>
                                 
-                                    <MDBCol md="5">
+                                    <MDBCol md="4">
                                         <Autocomplete
                                             limitTags={2}
                                             id="genes"
