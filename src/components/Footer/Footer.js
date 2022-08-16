73067e335efe4d7e3c66930782cc6f9a1561bde3
Laurence Noël <laurence.r.noel@gmail.com>
2022-08-16 15:03:47 +0000

    modified footer and background color

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,10 +24,10 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 function Footer() {
   return (
-    <MDBFooter className="font-small mt-4 bg-dark text-white">
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          HuDeCA - &copy; {new Date().getFullYear()} Copyright: <a href="https://www.scilicium.com"> SciLicium </a>
+    <MDBFooter className="font-small mt-4">
+      <div className="text-center py-3">
+        <MDBContainer fluid className="text-dark">
+          UNCOVER - &copy; {new Date().getFullYear()} Copyright: IRSET / <a href="https://www.scilicium.com"> SciLicium </a>
         </MDBContainer>
       </div>
     </MDBFooter>
