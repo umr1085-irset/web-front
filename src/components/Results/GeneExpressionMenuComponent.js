72afdc34295487ed1cfd489c7a7744b27043ddc3
PaulRivaud <paulrivaud.info@gmail.com>
2022-12-19 16:57:38 +0100

    scatter dropdown

--- src/components/Results/GeneExpressionMenuComponent.js
+++ src/components/Results/GeneExpressionMenuComponent.js
@@ -291,7 +291,7 @@ class GeneExpPlotMenuComponent extends Component {
                             onChange={this.handleChangeGeneSelection}
                             >
                             <MenuItem value="relevant" >Relevant genes</MenuItem>
-                            <MenuItem value="variance" >Top 10 variable genes</MenuItem>
+                            <MenuItem value="variance" >Most variable genes</MenuItem>
                             <MenuItem value="custom" >My selection</MenuItem>
                             </Select>
 
