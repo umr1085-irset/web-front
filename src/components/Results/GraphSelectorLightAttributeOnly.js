5c3802017af8eb69b34d8273f5509fcab7d541c8
PaulRivaud <paulrivaud.info@gmail.com>
2023-07-11 15:22:14 +0200

    fixed plot selection

---
+++ src/components/Results/GraphSelectorLightAttributeOnly.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Button,Divider, Typography, MenuItem } from '@material-ui/core';
+import { MDBRow, MDBCol, MDBContainer  } from "mdbreact";
+import Tooltip from '@material-ui/core/Tooltip';
+
+
+class GraphSelectorLightAttributeOnly extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading:true
+      };
+      this.updateGraph = this.updateGraph.bind(this)
+    }
+
+    updateGraph(attr, chart_type, event){
+        this.props.callbackUpdateGraph(attr, chart_type)
+    }
+
+    render() {
+      const attributes = this.props.attrs['ca']
+      const display_type = 'scatter'
+
+      let tagList = attributes.map((attribute,idx)  => 
+        <MDBCol  key={this.props.name+"_2_col1_btn"+idx}>
+            <MDBRow middle key={this.props.name+"_2_col1_r1"+idx}>
+                    <Button onClick={(e) => this.updateGraph(attribute,'scatter')} id={this.props.name+"_2_btn"+idx} style={{ textTransform: 'none' }}>{attribute}</Button>
+             </MDBRow>
+         
+        </MDBCol>        
+        
+    )
+
+    return (
+  <MDBContainer fluid>
+            {tagList}
+     </MDBContainer>
+          
+       
+
+        );
+    }
+}
+export default GraphSelectorLightAttributeOnly;
