175c2079507e72ad9618b5923e321c658b636644
PaulRivaud <paulrivaud.info@gmail.com>
2023-08-04 08:38:32 +0200

    fill

--- src/components/Table/TableGenomeBrowser.js
+++ src/components/Table/TableGenomeBrowser.js
@@ -92,7 +92,7 @@ class TableGenomeBrowserComponent extends Component {
     })
     
   render() {
-        //const rows = this.props.rows
+        const rows = this.state.species
         //const species = this.state.species
         console.log(this.state)
         const columns = [
@@ -109,7 +109,7 @@ class TableGenomeBrowserComponent extends Component {
                 customBodyRenderLite: (dataIndex, rowIndex) => {
                   return (
                     
-                    <Box style={{width:200, maxWidth:240}}>{this.state.species[rowIndex].name}</Box>
+                    <Box style={{width:200, maxWidth:240}}>{rows[rowIndex].name}</Box>
                   
                   );
                 }
@@ -128,7 +128,7 @@ class TableGenomeBrowserComponent extends Component {
                 customBodyRenderLite: (dataIndex, rowIndex) => {
                   return (
                     
-                    <Box style={{width:200, maxWidth:240}}>{this.state.species[rowIndex].base_rgv_url}</Box>
+                    <Box style={{width:200, maxWidth:240}}>{rows[rowIndex].base_rgv_url}</Box>
                   
                   );
                 }
@@ -149,7 +149,7 @@ class TableGenomeBrowserComponent extends Component {
                   
                   //<Box style={{width:200, maxWidth:240}}>{this.state.species[rowIndex].short}</Box>
                   //<a class="btn btn-info"href="{{species[rowIndex].rgv_url}}">
-                  <Button variant="contained" href="{{this.state.species[rowIndex].rgv_url}}">
+                  <Button variant="contained" href="{{rows[rowIndex].rgv_url}}">
                     <i class="fa fa-external-link-alt" aria-hidden="true"></i>
                   </Button>
                 );
